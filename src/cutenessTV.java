37c7c5d9d5db759e655c09e4f98ca300dc8a5309
GrantBravo29 <league@WTS-IM-17.attlocal.net>
2019-03-17 17:25:41 -0700

    i finished cuteness tv and jack in the box

--- src/cutenessTV.java
+++ src/cutenessTV.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,6 +10,26 @@ public class cutenessTV implements ActionListener {
 
 	public static void main(String[] args) {
 		new cutenessTV().createUI();
+	}
+	void showDucks() {
+	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+	}
+
+	void showFrog() {
+	     playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+	}
+
+	void showFluffyUnicorns() {
+	     playVideo("https://www.youtube.com/watch?v=a-xWhG4UU_Y");
+	}
+
+	void playVideo(String videoID) {
+	     try {
+	          URI uri = new URI(videoID);
+	          java.awt.Desktop.getDesktop().browse(uri);
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
 	}
 
 	JButton leftButton = new JButton();
@@ -37,7 +58,15 @@ public class cutenessTV implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		
+		if (e.getSource()==leftButton) {
+			showDucks();
+		}
+		if (e.getSource()==rightButton) {
+			showFrog();
+		}
+		if (e.getSource()==middleButton) {
+			showF();
+		}
 	}}
 
 
--- src/jack.java
+++ src/jack.java
@@ -1,10 +1,11 @@
-import java.awt.Dimension;
+import java.applet.AudioClip;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.print.DocFlavor.URL;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JApplet;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,33 +15,82 @@ public class jack implements ActionListener {
 
 	public static void main(String[] args) {
 		new jack().createUI();
+
+
 	}
-
-	JButton leftButton = new JButton();
-	JButton rightButton = new JButton();
-
-	Dimension BIG = new Dimension(400, 400);
-	Dimension SMALL = new Dimension(200, 200);
-
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-
-	void createUI() {
-
-		frame.add(panel);
-		frame.setVisible(true);
-		leftButton.setText("surprise");
-		
-
-		leftButton.addActionListener(this);
-		rightButton.addActionListener(this);
-		panel.add(leftButton);
-		panel.add(rightButton);
-		frame.pack();
-		frame.setTitle("Demanding Buttons");
-	}
-
 	
+	private void showPicture(String fileName) { 
+	     try {
+	          JLabel imageLabel = createLabelImage(fileName);
+	          JFrame frame = new JFrame();
+	          frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	          frame.add(imageLabel);
+	          frame.setVisible(true);
+	          frame.pack();
+	     } catch (Exception e) {
+	          e.printStackTrace();
+	     }
+	}
+
+	private JLabel createLabelImage(String fileName) {
+	     try {
+	          java.net.URL imageURL = getClass().getResource(fileName);
+	          if (imageURL == null) {
+	               System.err.println("Could not find image " + fileName);
+	               return new JLabel();
+	          } else {
+	               Icon icon = new ImageIcon(imageURL);
+	               JLabel imageLabel = new JLabel(icon);
+	               return imageLabel;
+	          }
+	     } catch (Exception e) {
+	          System.err.println("Could not find image " + fileName);
+	          return new JLabel();
+	     }
+	}
+		
+		JButton middleButton = new JButton();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		int clicks = 0;
+		private void createUI() {
+
+			
+			middleButton.setText("surprise");
+			
+			middleButton.addActionListener(this);
+			
+			panel.add(middleButton);
+			frame.add(panel);
+			frame.pack();
+			frame.setTitle("jack");
+			frame.setVisible(true);
+			
+		}
+		private void playSound(String soundFile) { 
+		     try {
+		          AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+		          sound.play();
+		     } catch (Exception e) {
+		          e.printStackTrace();
+		     }
+		}
+
+	@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+
+			if(e.getSource()==middleButton) {
+				clicks++;
+if(clicks==5){
+	showPicture("jackInTheBox.png");
+}
+}
+		}}
 
 		
-	}
+
+
+
+
+
